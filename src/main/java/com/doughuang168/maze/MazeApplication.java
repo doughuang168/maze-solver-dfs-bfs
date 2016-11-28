ac5c3f25b1cee407bc57dccc643524fa57935f2b
Doug Huang <doug.huang@hotmail.com>
2016-11-27 16:09:27 -0800

    add MazeData.java to process user supply maze

--- src/main/java/com/doughuang168/maze/MazeApplication.java
+++ src/main/java/com/doughuang168/maze/MazeApplication.java
@@ -2,9 +2,15 @@ package com.doughuang168.maze;
 
 import com.doughuang168.maze.Maze;
 
+import com.doughuang168.maze.MazeData;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class MazeApplication {
 
@@ -31,11 +37,44 @@ public class MazeApplication {
                 {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'e', 'x', 'x', 'x'}
         };
 
+
+        String mazePath = "";
+
+        ObjectMapper mapper = new ObjectMapper();
+        ;
+        MazeData mazeData = null;
+        char[][] mazeAryData = null;
+        //mapper = new ObjectMapper();
+        if (args.length == 1) {
+            mazePath = args[0];
+            try {
+                mazeData = mapper.readValue(new File(mazePath), MazeData.class);
+                mazeAryData = mazeData.getMazeArray();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                System.out.println();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println();
+            }
+        }
+
+
         int startI=0, startJ=2;
         int endI=15, endJ=12;
 
+        MazeGraph mazeGraph;
+
         //MazeGraph initialization
-        MazeGraph mazeGraph = new MazeGraph( maze, startI, startJ, endI, endJ);
+        if (mazePath == "") {//use built in default maze
+            mazeGraph = new MazeGraph( maze, startI, startJ, endI, endJ);
+        } else {
+            mazeGraph = new MazeGraph( mazeAryData, mazeData.getStartI(), mazeData.getStartJ(), mazeData.getEndI(), mazeData.getEndJ());
+        }
+
+
+        //MazeGraph initialization
+        //MazeGraph mazeGraph = new MazeGraph( maze, startI, startJ, endI, endJ);
 
         mazeGraph.print();
         System.out.println();
---
+++ src/main/java/com/doughuang168/maze/MazeData.java
@@ -0,0 +1,47 @@
+package com.doughuang168.maze;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MazeData {
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Row {
+        List<String> mazeRow;
+        public List<String> getMazeRow() {return mazeRow;}
+    }
+    private Integer startI;
+    private Integer startJ;
+    private Integer endI;
+    private Integer endJ;
+
+    List<Row> maze;
+
+    public Integer getStartI() {return startI;};
+    public Integer getStartJ() {return startJ;};
+    public Integer getEndI() {return startI;};
+    public Integer getEndJ() {return startI;};
+    public List<Row> getMaze() {return maze;};
+    public Integer size() { return maze.size();}
+    public char[][] getMazeArray() {
+        char[][] mazeArray=new char[size()][size()];
+
+        int i=0;
+        for (Row row: maze) {
+            int j=0;
+            List<String> strList = row.getMazeRow();
+            for (String str: strList) {// iter size() times
+                char[] charArray = str.toCharArray();
+                mazeArray[i][j] = charArray[0]; //alway use charArray[0]
+                j++;
+            }
+            i++;
+        }
+
+        return mazeArray;
+    }
+}
+
